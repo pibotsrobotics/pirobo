c04f15545796f23c62ba81bca19794ef635bdc44
Shadin Kappachali <iedc25to26@gmail.com>
2026-04-25 15:06:19 +0530

    Update logo to Pibot logo.png

--- frontend/src/components/ui/Logo.jsx
+++ frontend/src/components/ui/Logo.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Logo = ({ className = "h-8 w-auto", ...props }) => {
     return (
         <img 
-            src="/2.png.png" 
+            src="/Pibot%20logo.png" 
             alt="Pi Bots Logo" 
             className={className}
             {...props}
